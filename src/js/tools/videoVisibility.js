8887f7d8f5af72c3a3a52c7d66c343c644ad06a9
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2020-12-01 13:21:03 +0100

    fix mobile index.html

--- src/js/tools/videoVisibility.js
+++ src/js/tools/videoVisibility.js
@@ -1,17 +1,19 @@
+import mobileBG from "../../assets/index-min-06.jpg";
+
 window.addEventListener("DOMContentLoaded", () => {
   const video = document.querySelector("video");
   const header = document.querySelector("header");
 
   if (window.innerWidth < 800) {
     video.style.display = "none";
-    header.style.background = "url(./src/assets/index-min-06.jpg)";
+    header.style.background = `url(${mobileBG})`;
   }
 
   window.addEventListener("resize", () => {
     if (window.innerWidth < 800) {
       video.style.display = "none";
 
-      header.style.background = "url(./src/assets/index-min-06.jpg)";
+      header.style.background = `url(${mobileBG})`;
     } else {
       video.style.display = "";
       header.style.background = "";
